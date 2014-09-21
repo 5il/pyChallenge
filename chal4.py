6657340d49081beb8a6cdb9ba08a1f29403bd0d1
5il <kmcgowa2@gmu.edu>
2014-09-21 16:52:01 -0400

    moved to requests library and cleaned up output

--- chal4.py
+++ chal4.py
@@ -1,13 +1,11 @@
-# Need to make this less sloppy...
-
 import re
-import urllib
+import requests
 import sys
 
-f = urllib.urlopen("http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="+sys.argv[1])
-n = re.search("(and the next nothing is )(\d+)", f.read())
-print n.group(0)
+page = requests.get("http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="+sys.argv[1]).text
+n = re.search("(and the next nothing is )(\d+)", page)
+print page
 while n:
- f = urllib.urlopen("http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="+n.group(2))
- n = re.search("(and the next nothing is )(\d+)", f.read())
- print n.group(0)
+ page = requests.get("http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="+n.group(2)).text
+ n = re.search("(and the next nothing is )(\d+)", page)
+ print page
